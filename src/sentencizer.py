918833be16335e8d60f74e1bdf045b388435a690
Tiago Pimentel <tiagopms@gmail.com>
2020-03-07 08:15:44 +0000

    Change to compatibilize spacy and wikipedia language codes

--- src/sentencizer.py
+++ src/sentencizer.py
@@ -2,10 +2,14 @@ import spacy
 
 
 class Sentencizer:
+    spacy_dict = {
+        'no': 'nb',  # Norwegian (Bokmål) has a different code in spacy
+    }
 
     def __init__(self, language):
         self.language = language
-        self.sentencizer = self.get_sentencizer(language)
+        self.spacy_language = self.spacy_dict.get(language, language)
+        self.sentencizer = self.get_sentencizer(self.spacy_language)
 
     @classmethod
     def get_sentencizer(cls, language):
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,10 +2,14 @@ import spacy
 
 
 class Tokenizer:
+    spacy_dict = {
+        'no': 'nb',  # Norwegian (Bokmål) has a different code in spacy
+    }
 
     def __init__(self, language):
         self.language = language
-        self.tokenizer = self.get_tokenizer(language)
+        self.spacy_language = self.spacy_dict.get(language, language)
+        self.tokenizer = self.get_tokenizer(self.spacy_language)
 
     @classmethod
     def get_tokenizer(cls, language):
