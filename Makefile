ff4cb88b6311fa775fb14cfd3fd9aa190d20d1a1
Tiago Pimentel <tiagopms@gmail.com>
2022-02-03 12:52:56 +0000

    Code to get wikipedia text from wiki40B

--- Makefile
+++ Makefile
@@ -4,8 +4,13 @@ INPUT_DIR := input
 OUTPUT_DIR := output
 MULTILINGUAL := False
 DUMP_SIZE := 10000
+IS_WIKI40B := True
 
 MULTILINGUAL_ARG := $(if $(filter-out $(MULTILINGUAL), False),--allow-multilingual,)
+
+TRUE_STR := True TRUE true
+# WIKI40B_LANGUAGES := en ar zh zh-cn zh-tw nl fr de it ja ko pl pt ru es th tr bg ca cs da el et fa fi he hi hr hu id lt lv ms no ro sk sl sr sv tl uk vi
+# WIKI_NAME := $(if $(filter $(LANGUAGE),$(WIKI40B_LANGUAGES)),wiki40,latest)
 
 INPUT_DIR_LANG := $(INPUT_DIR)/$(LANGUAGE)
 OUTPUT_DIR_LANG := $(OUTPUT_DIR)/$(LANGUAGE)
@@ -14,23 +19,44 @@ XML_NAME := $(LANGUAGE)wiki-latest-pages-articles.xml.bz2
 WIKIURL := https://dumps.wikimedia.org/$(LANGUAGE)wiki/latest/$(XML_NAME)
 JSON_NAME := wiki-latest.json.gz
 
-# Data Files
+
+# Raw Wiki Data Files
 XML_FILE := $(INPUT_DIR_LANG)/$(XML_NAME)
 JSON_FILE := $(INPUT_DIR_LANG)/$(JSON_NAME)
-WIKI_PARSED_FILE := $(OUTPUT_DIR_LANG)/parsed.txt
+WIKI_LATEST_FILE := $(OUTPUT_DIR_LANG)/parsed-latest.txt
+
+# Wiki40B Data Files
+WIKI40B_FILE := $(OUTPUT_DIR_LANG)/parsed-wiki40b.txt
+
+# Output File
+WIKI_FILE := $(if $(filter $(IS_WIKI40B),$(TRUE_STR)),$(WIKI40B_FILE),$(WIKI_LATEST_FILE))
 
 
-all: get_wiki
+all: get_tokenised
 
-get_wiki: $(WIKI_PARSED_FILE)
+ifneq ($(filter $(IS_WIKI40B),$(TRUE_STR)),)
+get_tokenised: $(WIKI40B_FILE)
+else
+get_tokenised: $(WIKI_LATEST_FILE)
+endif
 
-get_raw: $(XML_FILE)
+get_data: $(JSON_FILE)
 
-# Tokenize wikipedia
-$(WIKI_PARSED_FILE): $(JSON_FILE)
+download_raw: $(XML_FILE)
+
+
+
+# Tokenize wiki40b
+$(WIKI40B_FILE):
+	echo "Download data"
+	mkdir -p $(OUTPUT_DIR_LANG)
+	python src/tokenize_wiki_40b.py --language $(LANGUAGE) --raw-data-dir $(INPUT_DIR_LANG) --tokenized-file $(WIKI40B_FILE)
+
+# Tokenize wikipedia latest
+$(WIKI_LATEST_FILE): $(JSON_FILE)
 	echo "Tokenize data"
 	mkdir -p $(OUTPUT_DIR_LANG)
-	python src/get_tokens.py --wikipedia-raw-file $(JSON_FILE) --wikipedia-tokenized-file $(WIKI_PARSED_FILE) \
+	python src/tokenize_wiki_latest.py --json-file $(JSON_FILE) --tokenized-file $(WIKI_LATEST_FILE) \
 		--language $(LANGUAGE) --dump-size $(DUMP_SIZE) $(MULTILINGUAL_ARG)
 
 # Preprocess wikipedia to json
---
+++ src/tokenize_wiki_40b.py
@@ -0,0 +1,92 @@
+import argparse
+import re
+import math
+from tqdm import tqdm
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# from model import sentencizer, tokenizer
+from tokenize_wiki_latest import get_tokenizer, get_sentencizer, tokenize_sentence, write_txt
+
+# import ipdb; ipdb.set_trace()
+
+# # Construct a tf.data.Dataset
+# ds = tfds.load('wiki40b/nl', split='train', shuffle_files=True)
+# # Turn to a numpy df so that we can easily extract text
+# numpy_items = tfds.as_numpy(ds)
+# numpy_items['text']
+
+r1 = "_START_ARTICLE_\n[^_]*"
+r2 = "_START_PARAGRAPH_\n"
+r3 = "_START_SECTION_\n[^_]*"
+r4 = "_NEWLINE_"
+
+ARTICLE_REGEX = re.compile(f"({r1}|{r2}|{r3}|{r4})")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # Wikipedia
+    parser.add_argument(
+        "--raw-data-dir", type=str,
+        help="The file containing the wiki json files from which to read")
+    parser.add_argument(
+        "--tokenized-file", type=str,
+        help="The file containing the wiki json files from which to read")
+    parser.add_argument(
+        "--language", type=str, required=True,
+        help="The wikipedia language to get tokenizer.")
+    parser.add_argument("--batch-size", type=int, default=1024)
+    # parser.add_argument("--data_dir", help="path to save data files to")
+
+    return parser.parse_args()
+
+
+def process_batch(batch, sentencizer, tokenizer, pbar):
+    processed_articles = []
+    for item in batch.get("text"):
+        text = item.decode("UTF-8")
+        text_clean = re.sub(ARTICLE_REGEX, "\n", text)
+        paragraphs = [paragraph for paragraph in text_clean.split('\n') if paragraph != '']
+        sentences_raw = [sentence for paragraph in paragraphs for sentence in sentencizer(paragraph)]
+
+        token_lists = [tokenize_sentence(tokenizer, sentence) for sentence in sentences_raw]
+        sentences = '\n'.join(' '.join(sentence) for sentence in token_lists)
+        # import ipdb; ipdb.set_trace()
+
+        processed_articles += [sentences]
+        pbar.update(1)
+
+    return processed_articles
+
+
+def process_tf_dataset(ds, ds_size, tgt_fname, sentencizer, tokenizer):
+    # import ipdb; ipdb.set_trace()
+    with tqdm(total=ds_size, desc='Getting wiki40b dataset', mininterval=1) as pbar:
+        for batch in ds.as_numpy_iterator():
+            processed_articles = process_batch(batch, sentencizer, tokenizer, pbar)
+            write_txt(tgt_fname, processed_articles)
+
+
+def get_data(language, raw_data_dir, tgt_fname, batch_size, allow_multilingual=False):
+    sentencizer = get_sentencizer(language, allow_multilingual)
+    tokenizer = get_tokenizer(language, allow_multilingual)
+
+    for data_split in ['train', 'validation', 'test']:
+        # Download the dataset from tensorflow
+        ds, ds_info = tfds.load(f"wiki40b/{language}", split=data_split, data_dir=raw_data_dir,
+                       shuffle_files=False, batch_size=batch_size, with_info=True)
+        ds_size = ds_info.splits[data_split].num_examples
+        process_tf_dataset(ds, ds_size, tgt_fname, sentencizer=sentencizer, tokenizer=tokenizer)
+
+
+def main():
+    args = get_args()
+    print(args)
+
+    get_data(args.language, raw_data_dir=args.raw_data_dir,
+             tgt_fname=args.tokenized_file, batch_size=args.batch_size)
+
+
+if __name__ == '__main__':
+    main()
