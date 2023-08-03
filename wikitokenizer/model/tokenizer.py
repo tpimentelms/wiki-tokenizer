3f5fccd84980743fd4e6c0b8ecfe3c79e735ff60
Tiago Pimentel <tp472@cam.ac.uk>
2023-08-03 19:46:30 +0100

    When tokenizing long japanese text, do it in parts when input is too long

--- wikitokenizer/model/tokenizer.py
+++ wikitokenizer/model/tokenizer.py
@@ -9,4 +9,20 @@ class Tokenizer(SpacyBase):
         self.tokenizer = nlp.tokenizer
 
     def __call__(self, *args, **kwargs):
-        return self.tokenizer(*args, **kwargs)
+        try:
+            return self.tokenizer(*args, **kwargs)
+        except Exception as ex:
+            in_string = args[0]
+            if self.language == 'ja' and self.size_in_bits(in_string) > 49149:
+                tokens = in_string.split()
+                n_tokens = int(len(tokens) / 2)
+
+                first_half = [x for x in self(' '.join(tokens[:n_tokens]))]
+                second_half = [x for x in self(' '.join(tokens[n_tokens:]))]
+                return first_half + second_half
+
+            raise ex
+
+    @staticmethod
+    def size_in_bits(s):
+        return len(s.encode('utf-8'))
